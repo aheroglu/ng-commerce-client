fbafcfa3f1f82738829b6a7c238b05be2762b99f
aheroglu <ahmet20033134@gmail.com>
2024-12-18 13:37:20 +0300

    connection string server olarak güncellendi.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,11 @@
-export const apiUrl: string | null = 'https://localhost:7198/api';
-export const categoriesImageUrl: string | null = 'https://localhost:7198/images/categories/';
-export const productsImageUrl: string | null = 'https://localhost:7198/images/products/';
+// Local Connection:
+// export const apiUrl: string | null = 'https://localhost:7198/api';
+// export const categoriesImageUrl: string | null = 'https://localhost:7198/images/categories/';
+// export const productsImageUrl: string | null = 'https://localhost:7198/images/products/';
+
+// Server Connection:
+export const apiUrl: string | null = 'https://ng-commerce-api.aheroglu.dev/api';
+export const categoriesImageUrl: string | null =
+  'https://ng-commerce-api.aheroglu.dev/images/categories/';
+export const productsImageUrl: string | null =
+  'https://ng-commerce-api.aheroglu.dev/images/products/';
