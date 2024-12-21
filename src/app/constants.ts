d6b1c7001f4c85396aaa45340a3f4e02db1c0c74
aheroglu <ahmet20033134@gmail.com>
2024-12-21 14:12:58 +0300

    api url güncellendi.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,13 +1,13 @@
 // Local Connection:
-export const apiUrl: string | null = 'https://localhost:7198/api';
-export const categoriesImageUrl: string | null =
-  'https://localhost:7198/images/categories/';
-export const productsImageUrl: string | null =
-  'https://localhost:7198/images/products/';
+// export const apiUrl: string | null = 'https://localhost:7198/api';
+// export const categoriesImageUrl: string | null =
+//   'https://localhost:7198/images/categories/';
+// export const productsImageUrl: string | null =
+//   'https://localhost:7198/images/products/';
 
 //  Server Connection:
-// export const apiUrl: string | null = 'https://ng-commerce-api.aheroglu.dev/api';
-// export const categoriesImageUrl: string | null =
-//   'https://ng-commerce-api.aheroglu.dev/images/categories/';
-// export const productsImageUrl: string | null =
-//   'https://ng-commerce-api.aheroglu.dev/images/products/';
+export const apiUrl: string | null = 'https://ng-commerce-api.aheroglu.dev/api';
+export const categoriesImageUrl: string | null =
+  'https://ng-commerce-api.aheroglu.dev/images/categories/';
+export const productsImageUrl: string | null =
+  'https://ng-commerce-api.aheroglu.dev/images/products/';
